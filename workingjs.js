8de105f0508c47fe9c2195672701b968fa78224d
Fifi55555 <91993494+Fifi55555@users.noreply.github.com>
2021-10-06 00:49:54 +0300

    Add files via upload

---
+++ workingjs.js
@@ -0,0 +1,7 @@
+<script>
+var a = document.createElement('iframe');
+a.src = "https://handytricksandtips.blogspot.com"; //add your iframe path here
+a.width = "1000";
+a.height = "500";
+document.querySelector('body').appendChild(a);
+</script>
